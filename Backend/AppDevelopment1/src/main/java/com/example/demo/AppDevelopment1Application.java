6d4ce77dc27419651a43d1d7b3aa458ddd5b6a90
SUMANADITHYA <113884434+SUMANADITHYA@users.noreply.github.com>
2023-07-28 11:47:42 +0530

    Final Project Appdevelopment/BackEnd, MicroService

---
+++ Backend/AppDevelopment1/src/main/java/com/example/demo/AppDevelopment1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppDevelopment1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppDevelopment1Application.class, args);
+	}
+
+}
